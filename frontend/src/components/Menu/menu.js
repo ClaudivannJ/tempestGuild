9a95d72f7c01f1aa06b85c3d579f8a3c44c080a6
Claudivannj <claudivan.ar2002@gmail.com>
2024-08-08 18:16:22 -0300

    Adiciona funcionalidade de logout no componente Menu

--- frontend/src/components/Menu/menu.js
+++ frontend/src/components/Menu/menu.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './Menu.module.css';
 
 const Menu = ({ isOpen }) => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+    };
   return (
     <nav className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
       <ul>
@@ -10,7 +15,7 @@ const Menu = ({ isOpen }) => {
         <li><Link to="/tournaments">Tournaments</Link></li>
         <li><Link to="/create-tournament">Create Tournament</Link></li>
         <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/logout">Logout</Link></li>
+        <li><button onClick={handleLogout} className={styles.logoutButton}>Encerrar Sessão</button></li>
       </ul>
     </nav>
   );
